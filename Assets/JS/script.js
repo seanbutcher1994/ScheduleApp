85290fe99050c128f609b6c5fd1c8f0d080e4d12
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2022-09-10 15:36:38 +0800

    Add comments for JS and mockup of HTML

---
+++ Assets/JS/script.js
@@ -0,0 +1,24 @@
+/* <div class="row align-items-center time-row">
+<div class="col-2 d-flex justify-content-center time-col-time">9am</div>
+<div class="col-8 time-col-textarea">
+  <textarea name="" id="" cols="30" rows="2"></textarea>
+</div>
+<div class="col-2">
+  <button type="submit" class="btn btn-primary">Save</button>
+</div>
+</div> */
+
+
+// When user lands on this app
+    // should see the current time (clock)
+    // should see 9am-5pm blocks with colour blocks (past, present and future)
+    // with all the previous notes
+
+// When user click on submit button
+    // should get the note written on the target timeblock
+    // save the note to local storage (based on the time)
+
+
+// scenarios
+    // can the user edit past timeblocks
+    // can the user edit current timeblock
