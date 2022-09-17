62faa0732f5e19bfb2763284511475b9413d3288
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2022-09-17 12:57:59 +0800

    refactor code for adding elements etc as wasn't working for me... add localstorage functionality

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,63 +1,101 @@
-/* <div class="row align-items-center time-row">
-<div class="col-2 d-flex justify-content-center time-col-time">9am</div>
-<div class="col-8 time-col-textarea">
-  <textarea name="" id="" cols="30" rows="2"></textarea>
-</div>
-<div class="col-2">
-  <button type="submit" class="btn btn-primary">Save</button>
-</div>
-</div> */
 
-function createTimeBlock(time, content =""){
-
-    const timeRow = $('<div class="row align-items-center time-row">');
-
-    const colTime = $('<div class="col-2 d-flex justify-content-center time-col-time">').text(time + ":00")
-
-    const colTextArea = $('<div class="col-8 time-col-textarea">');
-    const textArea = $(' <textarea cols="30" rows="2">').val(content)
+var time = moment();
+var currentTime = $('#currentTime');
+var displayContainer = $('.container');
 
 
-    colTextArea.append(textArea);
 
-    const colButton = $('<div class="col-2">')
-    const button = $('<button type="submit" class="btn btn-primary">').text("Save")
-    colButton.append(button);
-
-    return timeRow.append(colTime, colTextArea, colButton);
-}
-
-// Function for User to be able to see current date and time
-$(function(){
-    const currentTime = $('#currentTime');
-    const container = $(".container")
-    function startTimer(){
-        setInterval(function(){
-            currentTime.text(moment().format('MMM, Do, YYYY HH:mm:ss'));
-        }, 1000);
-    }
-    startTimer();
+ // create element for a timeblock (input element)
+function dispayTime(content=""){
+    var rightNow = moment().format('[Today is] dddd, MMM Do, YYYY');
+    currentTime.text(rightNow);
     
-    
-    for (let index = 9; index < 18; index++) {
+     // create 9 of said element between 9&5
+    for (let i = 9; i < 18; i++) {
+        var timeRow = $('<div class="row align-items-center time-row">');
+        var colTime = $('<div class="col-2 d-flex justify-content-center time-col-time">').text(i + ':00');
+        var colTextArea = $('<div class="col-8 time-col-textarea">');
+        var textArea = $('<textarea name="" id="text" cols="30" rows="2">').val(content);
+        var colSaveBtn = $('<div class="col-2 time-col-button">');
+        var saveBtn = $('<button type="submit" id="submitBtn" class="saveBtn btn btn-primary">').text('Save')
 
-        const previousNotes = "";
-        const timeBlock = createTimeBlock(index, previousNotes);
-    
-        container.append(timeBlock);
+        displayContainer.append(
+            timeRow.append(
+                colTime,
+                colTextArea.append(textArea),
+                colSaveBtn.append(saveBtn),
+            ),
+            
+            
+            );
+            
+        var now = Number(time.format("H"));
+        console.log(now);
+        var timeBlock = i;
+        console.log(timeBlock);
+        // define variable for what past/present/future is
+        // if time is before the future
+        isFuture = (now < timeBlock);
+        isPast = (now > timeBlock);
+        isPresent = (now == timeBlock);
+            // logic below applys classes based on the above variable definitions
+            if(isPast){
+                textArea.addClass("past");
+            }
+           
+            if(isFuture){
+                textArea.addClass("future");
+            } 
+            
+            if(isPresent){
+                textArea.addClass("present");
+            }
         
     }
+    console.log(time);
+ 
+    
+}
+
+
+// when i press the save button, the text saves to local storage
+    // add save button to html
+    // activate the save button
+    // get text content - let toave =  $('#input1').text 
+    // format it - .JSON
+    // save it local storage - setLocalStorage
+let contents = [];
+function saveInput(event){
+    event.preventDefault();
+    let content = {
+        id: Date.now(),
+        input: document.getElementById('text').value
+    }
+    contents.push(content);
+
+    console.log('added', {contents});
+    let pre = document.querySelector('#text');
+    pre.textContent = JSON.stringify(contents);
+
+    localStorage.setItem('myContent', JSON.stringify(contents));
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('submitBtn').addEventListener('click', saveInput);
 })
 
-// When user lands on this app
-    // should see 9am-5pm blocks with colour blocks (past, present and future)
-    // with all the previous notes
-
-// When user click on submit button
-    // should get the note written on the target timeblock
-    // save the note to local storage (based on the time)
 
 
-// scenarios
-    // can the user edit past timeblocks
-    // can the user edit current timeblock
+
+
+
+
+//TODO:
+    
+// when I refresh the page, the saved events stay.
+    // getLocalStorage
+
+    // when first loading page, we need to check localstorage first. *** 
+
+
+    dispayTime();
